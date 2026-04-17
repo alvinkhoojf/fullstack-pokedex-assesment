15a4c62e9c13324b9fd0fc9ed8f6a6384edc2bd0
ALVIN-DESKTOP\Alvin <alvinkhoojf@gmail.com>
2026-04-17 19:50:21 +0800

    update: successfully called PokeAPI

--- backend/app/Http/Controllers/PokemonController.php
+++ backend/app/Http/Controllers/PokemonController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PokemonController extends Controller
 {
@@ -11,11 +12,25 @@ class PokemonController extends Controller
         $limit = $request->query('limit', 10);
         $offset = ($page - 1) * $limit;
 
-
-        return response()->json([
-            'page' => $page,
+        $response = Http::get('https://pokeapi.co/api/v2/pokemon', [
             'limit' => $limit,
             'offset' => $offset
+        ]);
+
+        if ($response->failed()) {
+            return response()->json([
+                'message' => 'Failed to fetch API'
+            ], 500);
+        }
+
+        $data = $response->json();
+
+        return response()->json([
+            'message' => 'Pokemon fetched succesfully',
+            'page' => $page,
+            'limit' => $limit,
+            'offset' => $offset,
+            'data' => $data
         ]);
     }
 }
